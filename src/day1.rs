ba940a97eb2f51e54b56364801ceaaaea06b5754
Tim Haemisch <haemtim@gmail.com>
2023-12-01 10:40:40 +0100

    day one part one

---
+++ src/day1.rs
@@ -0,0 +1,93 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+pub fn main(part: u32) -> io::Result<()> {
+    match part {
+        1 => part_one(),
+        2 => part_two(),
+        _ => {
+            println!("Invalid part");
+            return Err(io::Error::new(io::ErrorKind::InvalidInput, "Invalid part"));
+        }
+    }
+}
+
+fn part_one() -> io::Result<()> {
+    let mut total: i32 = 0;
+
+    let file = File::open("/home/tim/prog/adventofcode2023/src/inputs/day1.txt")?;
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line?;
+        let mut numbers = String::new();
+        for c in line.chars() {
+            if c.is_digit(10) {
+                numbers.push(c);
+            }
+        }
+        let mut first_plus_last = String::new();
+        first_plus_last.push(numbers.chars().nth(0).unwrap());
+        first_plus_last.push(numbers.chars().nth(numbers.len() - 1).unwrap());
+
+        let subtotal: i32 = first_plus_last.parse::<i32>().unwrap();
+        total += subtotal;
+    }
+
+    println!("{}", total);
+    Ok(())
+}
+
+fn part_two() -> io::Result<()> {
+    let mut total: i32 = 0;
+
+    let file = File::open("/home/tim/prog/adventofcode2023/src/inputs/day1.txt")?;
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line?;
+        let numbers: String = transform_string(&line);
+        let mut first_plus_last = String::new();
+        first_plus_last.push(numbers.chars().nth(0).unwrap());
+        first_plus_last.push(numbers.chars().nth(numbers.len() - 1).unwrap());
+
+        let subtotal: i32 = first_plus_last.parse::<i32>().unwrap();
+        total += subtotal;
+    }
+
+    println!("{}", total);
+    Ok(())
+}
+
+fn transform_string(input: &str) -> String {
+    let mut result = String::new();
+    let numbers = [
+        ("one", '1'),
+        ("two", '2'),
+        ("three", '3'),
+        ("four", '4'),
+        ("five", '5'),
+        ("six", '6'),
+        ("seven", '7'),
+        ("eight", '8'),
+        ("nine", '9'),
+    ];
+
+    let mut temp = String::new();
+    for c in input.chars() {
+        if c.is_digit(10) {
+            result.push(c);
+        } else if c.is_alphabetic() {
+            temp.push(c);
+            for &(num_str, num_char) in &numbers {
+                if temp.contains(num_str) {
+                    result.push(num_char);
+                    temp.clear();
+                    break;
+                }
+            }
+        }
+    }
+
+    result
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io;
+
+mod day1;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter day:");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    let number: u32 = input.trim().parse().expect("Invalid input");
+
+    println!("Enter part:");
+
+    let mut input_part = String::new();
+    io::stdin()
+        .read_line(&mut input_part)
+        .expect("Failed to read input");
+
+    let part: u32 = input_part.trim().parse().expect("Invalid input");
+
+    match number {
+        1 => {
+            if let Err(err) = day1::main(part) {
+                eprintln!("Error: {}", err);
+            }
+        }
+        _ => {
+            println!("Invalid day");
+        }
+    }
 }
